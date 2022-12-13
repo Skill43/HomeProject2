05eec87601dd1121a8e0bfba46d3ed8e3267e6d5
Илья <ilacikoridze357@gmail.com>
2022-12-13 16:24:09 +0300

    Палиандром

---
+++ Test1/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+
+System.Console.WriteLine("Введите палиандром: ");
+string N = Convert.ToString(System.Console.ReadLine());
+char[] s = N.ToCharArray();
+Array.Reverse(s);
+string P = new string(s);
+N = N == P ? "Число " + N + " является палиандромом" : "Число " + N + " НЕ является палиандромом";
+System.Console.WriteLine(N);
