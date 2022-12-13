5e05be52137e20a11cada0fe37c9731447f57122
Илья <ilacikoridze357@gmail.com>
2022-12-13 18:57:29 +0300

    палиандром

--- Test1/Program.cs
+++ Test1/Program.cs
@@ -4,10 +4,10 @@
 // 23432 -> да
 
 
-System.Console.WriteLine("Введите палиандром: ");
+System.Console.WriteLine("Введите палиндром: ");
 string N = Convert.ToString(System.Console.ReadLine());
 char[] s = N.ToCharArray();
 Array.Reverse(s);
 string P = new string(s);
-N = N == P ? N + " является палиандромом" : N + " НЕ является палиандромом";
+N = N == P ? N + " является палиндромом" : N + " НЕ является палиндромом";
 System.Console.WriteLine(N);
